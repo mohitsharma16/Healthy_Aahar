8d6a805c085aeec5b5471d7a1849abd5d58b004c
mohitsharma16 <mohitsharmaofficial16@gmail.com>
2025-05-01 15:22:55 +0530

    refactor: update the color and theme for the app

--- app/src/main/java/com/mohit/healthy_aahar/ui/theme/Theme.kt
+++ app/src/main/java/com/mohit/healthy_aahar/ui/theme/Theme.kt
@@ -34,7 +34,7 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun Healthy_AaharTheme(
+fun HealthyAaharTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
