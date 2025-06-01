d282f9fddf987a32a93cd0d2d1bad5a24cdbcf61
mohitsharma16 <mohitsharmaofficial16@gmail.com>
2025-06-01 15:57:49 +0530

    refactor: the weekly report data class updated

--- app/src/main/java/com/mohit/healthy_aahar/model/WeeklyReport.kt
+++ app/src/main/java/com/mohit/healthy_aahar/model/WeeklyReport.kt
@@ -1,9 +1,15 @@
 package com.mohit.healthy_aahar.model
 
 data class WeeklyReport(
-    val calories: List<Double>,
-    val protein: List<Double>,
-    val carbs: List<Double>,
-    val fats: List<Double>,
-    val dates: List<String>
+    val user_id: String,
+    val user_name: String,
+    val week_start: String,
+    val week_end: String,
+    val days_logged: Int,
+    val total_days: Int,
+    val weekly_totals: NutritionTotals,
+    val daily_averages: NutritionTotals,
+    val daily_breakdown: List<DailyBreakdown>,
+    val goal_analysis: GoalAnalysis,
+    val insights: List<String>
 )
