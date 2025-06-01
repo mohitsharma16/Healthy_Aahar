8bda1c45e952540225dc684c38813ca055a254d0
mohitsharma16 <mohitsharmaofficial16@gmail.com>
2025-06-01 15:58:16 +0530

    refactor: update the backend url to render

--- app/src/main/java/com/mohit/healthy_aahar/network/RetrofitClient.kt
+++ app/src/main/java/com/mohit/healthy_aahar/network/RetrofitClient.kt
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
 
-    private const val BASE_URL = "https://healthyaaharbackend-production.up.railway.app/"
+    private const val BASE_URL = "https://healthy-aahar-backend.onrender.com/"
 
     val apiService: ApiService by lazy {
         Retrofit.Builder()
